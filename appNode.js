18887d41c8a5f1a0e9e5044448e0d345918b917c
PamelaCaiza11 <135941454+PamelaCaiza11@users.noreply.github.com>
2024-11-13 11:05:22 -0500

    feat: :ambulance: appnode.js

---
+++ appNode.js
@@ -0,0 +1,14 @@
+// Cargar el módulo Express
+const express = require('express');
+const app = express();
+
+// Configurar la ruta principal
+app.get('/', (req, res) => {
+  res.send('Hello World en Node JS');
+});
+
+// Configurar el puerto en el que la app va a escuchar
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
